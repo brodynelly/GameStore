2bf0e12019b09effa131f4b4f2820b154ef71c21
Brodynelly <brody2003@icloud.com>
2025-05-07 10:44:51 -0700

    updated some stuff

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
-import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
